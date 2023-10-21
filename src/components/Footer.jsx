2d52284a4faede47fc57d3ebcd444be57c04f09f
Elikin <elias.quinteros1998@gmail.com>
2023-10-21 00:05:36 -0300

    Footer mejoras

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,21 +3,36 @@ import fotoLinkedin from '../images/icons/linkedin.svg';
 import fotoGitHub from '../images/icons/github.svg';
 
 const Footer = () => {
+  const linkedinLink = 'https://www.linkedin.com/in/equinteros98/';
+  const githubLink = 'https://github.com/EQuinDev';
+
   return (
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto flex justify-between items-center">
         <p>Todos los derechos reservados | Año 2023</p>
         <div className="flex space-x-4">
-          <img
-            src={fotoLinkedin} 
-            alt="Logo de LinkedIn"
-            className="h-12"
-          />
-          <img
-            src={fotoGitHub} 
-            alt="Logo de GitHub"
-            className="h-12"
-          />
+          <a
+            href={linkedinLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={fotoLinkedin}
+              alt="Logo de LinkedIn"
+              className="h-12"
+            />
+          </a>
+          <a
+            href={githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={fotoGitHub}
+              alt="Logo de GitHub"
+              className="h-12"
+            />
+          </a>
         </div>
       </div>
     </footer>
